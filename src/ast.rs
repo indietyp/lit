7dad474ddc0d284dccdc61fce66749564cef2791
bmahmoud <bmahmoud@mpi-cbg.de>
2021-04-26 14:22:05 +0200

    add AST enums

---
+++ src/ast.rs
@@ -0,0 +1,71 @@
+pub enum ComparisonVerb {
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterEqual,
+    LessThan,
+    LessThanEqual,
+}
+
+pub enum OperatorVerb {
+    Plus,
+    Minus,
+    Multiply,
+}
+
+// TODO: UnaryExpression?
+pub enum ASTNode {
+    // Smallest Units
+    Identifier(String),
+    NaturalNumber(BigUInt),
+    Terms(Vec<ASTNode>),
+
+    // Assignment and Expressions
+    BinaryOperation {
+        verb: OperatorVerb,
+        lhs: Box<ASTNode>,
+        rhs: Box<ASTNode>,
+    },
+    Assignment {
+        lhs: Box<ASTNode>,
+        rhs: Box<ASTNode>,
+    },
+
+    // Control Structures
+    Loop {
+        ident: Box<ASTNode>,
+        terms: Box<ASTNode>,
+    },
+    While {
+        comp: Box<ASTNode>,
+        terms: Box<ASTNode>,
+    },
+}
+
+pub enum Macro {
+    AssignToVariable {
+        lhs: Box<ASTNode>,
+        rhs: Box<ASTNode>,
+    },
+    AssignToZero {
+        lhs: Box<ASTNode>,
+        rhs: Box<ASTNode>,
+    },
+    AssignToValue {
+        lhs: Box<ASTNode>,
+        rhs: Box<ASTNode>,
+    },
+    AssignToOpVariables {
+        lhs: Box<ASTNode>,
+        rhs: Box<ASTNode>,
+    },
+    AssignToOpValue {
+        lhs: Box<ASTNode>,
+        rhs: Box<ASTNode>,
+    },
+}
+
+pub enum PollutedASTNode {
+    ASTNode(ASTNode),
+    Macro(Macro),
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate pest;
+
+#[macro_use]
+extern crate pest_derive;
+
+mod ast;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct LoopParser;
+
 fn main() {
     println!("Hello, world!");
 }
