e1aa73b44dd09f135e5c05af3cc3f4ff3f55294f
botwalker <lukas.dams@protonmail.com>
2021-04-30 10:08:26 +0200

    Add package antd for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.less';
+import { Layout } from 'antd';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
+        <div className={'app'}>
+            <Layout>
+                <Layout>
+                    <Layout.Sider collapsed={true} collapsedWidth={0}>left sidebar</Layout.Sider>
+                    <Layout.Content>main content</Layout.Content>
+                    <Layout.Sider collapsed={true} collapsedWidth={0}>right sidebar</Layout.Sider>
+                </Layout>
+                <Layout.Footer>footer</Layout.Footer>
+            </Layout>
         </div>
     );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+    plugins: [reactRefresh()],
+    css: {
+        preprocessorOptions: {
+            less: {
+                modifyVars: { '@primary-color': '#1DA57A' },
+                javascriptEnabled: true,
+            }
+        }
+    }
+});
