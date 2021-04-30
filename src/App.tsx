719ddca1286388863b872be538aa909c5abb625b
botwalker <lukas.dams@protonmail.com>
2021-04-30 16:48:29 +0200

    Add very basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
 import './App.scss';
 import { Editor } from './modules/editor/Editor';
 
 function App() {
     return (
-        <div className={'app'}>
-            <h2>LOOP (Just) In Time</h2>
-            <Editor/>
-        </div>
+        <Container className={'app pt-3'}>
+            <Row>
+                <Col xs={12} md={6}>
+                    <h2>LOOP (Just) In Time</h2>
+                    <Editor/>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
