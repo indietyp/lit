83d50077d214be8ae63b5c8534e564b6e4a9faab
botwalker <lukas.dams@protonmail.com>
2021-05-11 15:56:41 +0200

    Switch to halfmoon ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { createContext, useContext } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 
-import './App.scss';
+import './App.css';
 import { LoopEditor } from './components/loopEditor/LoopEditor';
 import { Editor } from './store/Editor';
 
@@ -16,14 +15,14 @@ export const useEditor = () => useContext(TimerContext);
 function App() {
     return (
         <TimerContext.Provider value={editor}>
-            <Container className={'app pt-3'}>
-                <Row>
-                    <Col xs={12} md={6}>
+            <div className={'container app pt-3'}>
+                <div className={'row'}>
+                    <div className={'col-xs-12 col-md-6'}>
                         <h2>LOOP (Just) In Time</h2>
                         <LoopEditor/>
-                    </Col>
-                </Row>
-            </Container>
+                    </div>
+                </div>
+            </div>
         </TimerContext.Provider>
     );
 }
