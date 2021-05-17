644135a4c9a14847aff09537061a4ebb4621d7bc
bmahmoud <bmahmoud@mpi-cbg.de>
2021-05-17 10:53:27 +0200

    chore: reorder CompileFlags

--- engine/src/flags.rs
+++ engine/src/flags.rs
@@ -4,21 +4,28 @@ bitflags! {
     #[wasm_bindgen]
     pub struct CompileFlags: u16 {
         //-- Language Features --//
-        const LOOP       = 0b0000_0001;
-        const WHILE      = 0b0000_0010;
-
-        //-- Optimization Features --//
-        // enable dedicated zero variable (needs const variable)
-        const OPT_ZERO = 0b0001_0100_0000;
+        const LOOP          = 0b0001;
+        const WHILE         = 0b0010;
 
         //-- Configuration --//
         // instead of rewriting the LNO on compilation, let them stay pre expansion
-        const CNF_RETAIN_LNO = 0b0001_0000;
-        // Strict mode will no longer rewrite LOOP into WHILE, and macros are disabled.
-        const CNF_STRICT_MODE = 0b0010_0000;
-        // enable const variables
-        // (assignment to CONST declared variables is forbidden - internal only)
-        const CNF_CONST = 0b0100_0000;
+        const CNF_RETAIN_LNO = 0b0001 << 4;
+        // enable const variables (Assignment to CONST var is forbidden)
+        const CNF_CONST      = 0b0010 << 4;
+
+        //-- Optimization Features --//
+        // enable dedicated zero variable (needs const conf enabled)
+        const OPT_ZERO       = 0b0001 << 8 | Self::CNF_CONST.bits;
+
+        //-- Strict Mode --//
+        // Disable Macro Expansion
+        const STRCT_NO_MACRO = 0b0001 << 12;
+        // Disable Functions
+        const STRCT_NO_FUNC  = 0b0010 << 12;
+        // Disable Loop lowering to WHILE
+        const STRCT_NO_LPLWR = 0b0100 << 12;
+        // Enable complete strict mode
+        const STRCT          = 0b1111 << 12;
 
         //-- Compound Enum --//
         const LOOP_AND_WHILE = Self::LOOP.bits | Self::WHILE.bits;
