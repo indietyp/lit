fddef364b03ab5a4cf17485779d16ff4f3efa5dd
botwalker <lukas.dams@protonmail.com>
2021-05-24 17:13:37 +0200

    Add params argument to Interpreter.runProgram

--- src/store/Interpreter.test.ts
+++ src/store/Interpreter.test.ts
@@ -1,13 +1,35 @@
 import { Interpreter } from './Interpreter';
 import init from '../../engine/pkg';
+import { mockFetchWasmFile } from '../test-utils/mockFetchWasmFile';
 
-it('runs program', async () => {
+beforeAll(async () => {
+    mockFetchWasmFile();
     await init('lit_bg.wasm');
-    const interpreter = new Interpreter();
-    const programCode = "x := 1\nLOOP x DO\nx := x + 1\nEND";
-
-
-    const result = interpreter.runProgram(programCode);
-
-    expect(result['x'][0]).toBe(2);
 })
+
+describe('Interpreter', () => {
+    let interpreter!: Interpreter;
+
+    beforeEach(() => {
+        interpreter = new Interpreter();
+    })
+
+    it('runs program code w/o input parameters', () => {
+        const programCode = "x := 1\nLOOP x DO\nx := x + 1\nEND";
+
+        const result = interpreter.runProgram(programCode);
+
+        // @ts-ignore
+        expect(result['x'][0]).toBe(2);
+    });
+
+    it('runs program code w/ parameters', () => {
+        const programCode = "LOOP x DO\nx := x + 1\nEND";
+        const params: Map<string, number> = new Map(Object.entries({ x: 3 }));
+
+        const result = interpreter.runProgram(programCode, params);
+
+        // @ts-ignore
+        expect(result['x'][0]).toBe(6);
+    })
+});
--- src/store/Interpreter.ts
+++ src/store/Interpreter.ts
@@ -3,19 +3,18 @@ import { Program } from './Program';
 import init, { Builder, Runtime } from '../../engine/pkg';
 
 export class Interpreter {
-    public variableMap: Map<string, number> = new Map();
     public currentRuntime: Runtime | null = null;
 
     constructor() {
         // Load WASM Code
-        // if (import.meta.env.MODE !== 'test') {
-        //     init();
-        // }
+        if (import.meta.env.MODE !== 'test') {
+            init();
+        }
     }
 
-    public runProgram(programCode: string) {
+    public runProgram(programCode: string, params: Map<string, number> = new Map()) {
         const program = new Program(programCode);
-        this.currentRuntime = Builder.exec(program!.exec, this.variableMap);
+        this.currentRuntime = Builder.exec(program!.exec, params);
 
         while (this.currentRuntime.is_running()) {
             this.currentRuntime.step();
